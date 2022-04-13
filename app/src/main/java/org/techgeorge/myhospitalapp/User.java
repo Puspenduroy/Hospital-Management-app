adc9e12ef955b8d809157593b095ba7388dd3a00
Puspendu19BEC0708 <sam.itk3000000@gmail.com>
2022-04-13 23:56:36 +0530

    first commit

---
+++ app/src/main/java/org/techgeorge/myhospitalapp/User.java
@@ -0,0 +1,18 @@
+package org.techgeorge.myhospitalapp;
+
+public class User {
+
+    String firstName, lastName, age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+}
